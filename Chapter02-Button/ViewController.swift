aa02190fd8e4dd4f9c8a29c2c5818d4853625849
sobamemil <sieh96@naver.com>
2021-01-19 16:31:43 +0900

    프로그래밍을 이용하여 버튼 구성

--- Chapter02-Button/ViewController.swift
+++ Chapter02-Button/ViewController.swift
@@ -12,8 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 버튼 객체를 생성하고, 속성을 설정
+        let btn = UIButton(type: .system)
+        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)
+        
+        // 버튼을 수평 중앙 정렬
+        btn.center = CGPoint(x: self.view.frame.size.width / 2, y: 100)
+        btn.setTitle("테스트 버튼", for: .normal)
+        
+        // 루트 뷰에 버튼을 추가
+        self.view.addSubview(btn)
+        
+        // 버튼의 이벤트와 메소드 btnOnClick(_:)을 연결
+        btn.addTarget(self, action: #selector(btnOnClick(_:)), for: .touchUpInside)
     }
-
-
+    
+    @objc func btnOnClick(_ sender: Any) {
+        // 호출한 객체가 버튼이라면
+        if let btn = sender as? UIButton {
+            btn.setTitle("클릭되었습니다", for: .normal)
+        }
+    }
 }
 
