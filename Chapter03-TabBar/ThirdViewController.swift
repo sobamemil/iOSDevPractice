e0b5e77c90deaf95254cebc2d4d9ae5bb8f99936
sobamemil <sieh96@naver.com>
2021-01-20 14:21:14 +0900

    third view controller
    just add label

--- Chapter03-TabBar/ThirdViewController.swift
+++ Chapter03-TabBar/ThirdViewController.swift
@@ -8,5 +8,20 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        
+        title.text = "세 번쨰 탭"
+        title.textColor = UIColor.red
+        title.textAlignment = .center
+        title.font = UIFont.systemFont(ofSize: 14)
+        
+        title.sizeToFit()
+        title.center.x = self.view.frame.width / 2
+        
+        self.view.addSubview(title)
+    }
     
 }
