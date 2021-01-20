d8bdbbc7d978d872cec280840cfb71809055d00d
sobamemil <sieh96@naver.com>
2021-01-20 14:20:54 +0900

    second view controller
    just add label

--- Chapter03-TabBar/SecondViewController.swift
+++ Chapter03-TabBar/SecondViewController.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = UILabel(frame: CGRect(x: 0, y: 100, width: 100, height: 30))
+        
+        title.text = "두 번째 탭"
+        title.font = UIFont.systemFont(ofSize: 14) // 폰트는 System Font, 14pt
+        title.textColor = UIColor.red // 텍스트는 빨간색
+        title.textAlignment = .center // 레이블 내에서 중앙 정렬
+        
+        title.sizeToFit() // 콘텐츠의 내용에 맞게 레이블 크기 변경
+        title.center.x = self.view.frame.size.width / 2 // x 축의 중앙에 오도록
+        
+        self.view.addSubview(title)
+        
+        
+    }
 }
