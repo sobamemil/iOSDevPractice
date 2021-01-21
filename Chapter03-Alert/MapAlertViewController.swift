a257a3ff051452e9c7e9d8a4a86c5a4169bd71cd
sobamemil <sieh96@naver.com>
2021-01-21 13:10:01 +0900

    알림창에 슬라이더 구현

--- Chapter03-Alert/MapAlertViewController.swift
+++ Chapter03-Alert/MapAlertViewController.swift
@@ -29,6 +29,15 @@ class MapAlertViewController: UIViewController {
         imageBtn.addTarget(self, action: #selector(imageAlert(_:)), for: .touchUpInside)
         
         self.view.addSubview(imageBtn)
+        
+        // 슬라이더 알림창 버튼 생성
+        let sliderBtn = UIButton(type: .system)
+        sliderBtn.frame = CGRect(x: 0, y: 250, width: 100, height: 30)
+        sliderBtn.center.x = self.view.frame.size.width / 2
+        sliderBtn.setTitle("Slider Alert", for: .normal)
+        sliderBtn.addTarget(self, action: #selector(sliderAlert(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(sliderBtn)
     }
     
     @objc func mapAlert(_ sender: Any) {
@@ -60,6 +69,25 @@ class MapAlertViewController: UIViewController {
         // 콘텐츠 뷰 영역에 들어갈 뷰 컨트롤러 생성, 알림창에 등록
         let contentVC = ImageViewController()
         alert.setValue(contentVC, forKey: "contentViewController")
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func sliderAlert(_ sender: Any) {
+        // 콘텐츠 뷰 영역에 들어갈 뷰 컨트롤러를 생성
+        let contentVC = ControlViewController()
+        
+        // 경고창 객체를 생성
+        let alert = UIAlertController(title: nil, message: "이번 글의 평점을 입력해주세요.", preferredStyle: .alert)
+        
+        // 컨트롤 뷰 컨트롤러를 알림창에 등록
+        alert.setValue(contentVC, forKey: "contentViewController")
+        
+        // OK 버튼을 추가
+        let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
+            print(">>> sliderValue = \(contentVC.sliderValue)")
+        }
+        alert.addAction(okAction)
         
         self.present(alert, animated: true, completion: nil)
     }
