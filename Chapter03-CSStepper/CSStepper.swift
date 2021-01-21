ee89f4c504619fe2f9046a7d6a4ec0d65f8675be
sobamemil <sieh96@naver.com>
2021-01-21 17:27:29 +0900

    커스텀 스테퍼 구현 시작

---
+++ Chapter03-CSStepper/CSStepper.swift
@@ -0,0 +1,21 @@
+//
+//  CSStepper.swift
+//  Chapter03-CSStepper
+//
+//  Created by 심찬영 on 2021/01/21.
+//
+
+import UIKit
+
+class CSStepper: UIView {
+    // 스토리보드에서 호출할 초기화 메소드
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // 프로그래밍 방식으로 호출할 초기화 메소드
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+}
