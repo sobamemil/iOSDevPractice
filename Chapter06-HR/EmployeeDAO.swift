78295ef353a5dcaf5f03fcbb22f8b4866dec21b8
sobamemil <sieh96@naver.com>
2021-02-01 12:29:44 +0900

    FMDatabase 객체 생성 및 초기화

--- Chapter06-HR/EmployeeDAO.swift
+++ Chapter06-HR/EmployeeDAO.swift
@@ -33,5 +33,23 @@ class EmployeeDAO {
         var departTitle = "" // 소속 부서명
     }
     
-    
+    // FMDatabase 객체 생성 및 초기화
+    lazy var fmdb: FMDatabase! = {
+        // 파일 매니저 객체를 생성
+        let fileMgr = FileManager.default
+        
+        // 샌드박스 내 문서 디렉터리 경로에서 데이터베이스 파일을 읽어옴
+        let docPath = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first
+        let dbPath = docPath!.appendingPathComponent("hr.sqlite").path
+        
+        // 샌드박스 경로에 hr.sqlite 파일이 없다면 메인 번들에 만들어 둔 파일을 가져와 복사
+        if fileMgr.fileExists(atPath: dbPath) == false {
+            let dbSource = Bundle.main.path(forResource: "hr", ofType: "sqlite")
+            try! fileMgr.copyItem(atPath: dbSource, toPath: dbPath)
+        }
+        
+        // 준비된 데이터베이스 파일을 바탕으로 FMDatabase 객체를 생성
+        let db = FMDatabase(path: dbPath)
+        return db
+    }()
 }
