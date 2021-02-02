bea42f3b6aaf6aeb2e5cf175f2179528e7f058cb
sobamemil <sieh96@naver.com>
2021-02-02 12:50:26 +0900

    EmployeeDAO - find 메소드에서 매개변수를 받도록 변경

--- Chapter06-HR/EmployeeDAO.swift
+++ Chapter06-HR/EmployeeDAO.swift
@@ -61,15 +61,18 @@ class EmployeeDAO {
         self.fmdb.close()
     }
     
-    func find() -> [EmployeeVO] {
+    func find(departCd: Int = 0) -> [EmployeeVO] {
         // 반환할 데이터를 담을 [EmployeeVO] 타입의 객체 정의
         var employeeList = [EmployeeVO]()
         
         do {
+            let condition = departCd == 0 ? "" : "WHERE Employee.depart_cd = \(departCd)"
+            
             let sql = """
                 SELECT emp_cd, emp_name, join_date, state_cd, department.depart_title
                 FROM employee
                 JOIN department On department.depart_cd = employee.depart_cd
+                \(condition)
                 ORDER BY employee.depart_cd ASC
             """
             
