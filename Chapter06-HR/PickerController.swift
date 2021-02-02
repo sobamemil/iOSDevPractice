25a1f15ef103acb17950e17804de53f291e7475b
sobamemil <sieh96@naver.com>
2021-02-02 11:39:13 +0900

    부서 선택용 피커 뷰 컨트롤러 구현

--- Chapter06-HR/PickerController.swift
+++ Chapter06-HR/PickerController.swift
@@ -32,6 +32,29 @@ class DepartPickerVC: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // 외부에서 뷰 컨트롤러를 참조할 때를 위한 사이즈를 지정
         let pWidth = self.pickerView.frame.width
         let pHeight = self.pickerView.frame.height
-        self.preferredCOntentSize = CGSize(width: pWidth, height: pHeight)
+        self.preferredContentSize = CGSize(width: pWidth, height: pHeight)
+    }
+    
+    // 피커 뷰에 표시될 전체 컴포넌트 개수
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // 특정 컴포넌트의 행 개수
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.departList.count
+    }
+    
+    // 피커 뷰의 각 행에 표시될 뷰를 결정하는 메소드
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var titleView = view as? UILabel
+        if titleView == nil {
+            titleView = UILabel()
+            titleView?.font = UIFont.systemFont(ofSize: 14)
+            titleView?.textAlignment = .center
+        }
+        
+        titleView?.text = "\(self.departList[row].departTitle)(\(self.departList[row].departAddr))"
+        return titleView!
     }
 }
