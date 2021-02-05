bbe88ae8b6129427f0fc64c338df9c12a0338aa0
sobamemil <sieh96@naver.com>
2021-02-05 13:08:56 +0900

    로그 타입 처리를 위한 객체 정의

--- Chapter07-CoreData/LogVC.swift
+++ Chapter07-CoreData/LogVC.swift
@@ -11,3 +11,20 @@ import CoreData
 class LogVC: UITableViewController {
     
 }
+
+public enum LogType: Int16 {
+    case create = 0
+    case edit = 1
+    case delete = 2
+}
+
+extension Int16 {
+    func toLogType() -> String {
+        switch self {
+        case 0 : return "생성"
+        case 1 : return "수정"
+        case 2 : return "삭제"
+        default: return ""
+        }
+    }
+}
