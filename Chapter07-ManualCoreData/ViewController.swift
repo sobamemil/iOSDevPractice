1625c97dbc9f873b3f8e90031d611099dd7ac8a2
sobamemil <sieh96@naver.com>
2021-02-05 14:07:04 +0900

    Initial Commit

---
+++ Chapter07-ManualCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter07-ManualCoreData
+//
+//  Created by 심찬영 on 2021/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
