1a76062acc59ea1b38ed9e707881a4ac5ad6fb1e
sobamemil <sieh96@naver.com>
2021-02-08 14:52:44 +0900

    GET 방식으로 현재 시간 알려주는 API 호출하기

--- Chapter08-APITest/ViewController.swift
+++ Chapter08-APITest/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var currentTime: UILabel!
+    
+    @IBAction func callCurrentTime(_ sender: UIButton) {
+        do {
+            // URL 설정 및 GET 방식으로 API 호출
+            let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/currentTime")
+            let response = try String(contentsOf: url!)
+            
+            // 읽어온 값을 레이블에 표시
+            self.currentTime.text = response
+            self.currentTime.sizeToFit()
+        } catch let e as NSError {
+            print(e.localizedDescription)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
