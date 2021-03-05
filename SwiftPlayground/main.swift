9fbd8b260651c6bb8f4aa52cfd0df6d98dd7fabc
sobamemil <sieh96@naver.com>
2021-03-05 21:48:54 +0900

    guard 문이 사용될 수 없는 경우 예시 코딩.

--- SwiftPlayground/main.swift
+++ SwiftPlayground/main.swift
@@ -923,12 +923,19 @@ let names: [String] = ["wizplan", "eric", "yagom", "jenny"]
 //greet(personInfo)
 //// Hello Jenny
 //// I hope the weather is nice in Korea
+//
+//func enterClub(name: String?, age: Int?) {
+//    guard let name: String = name, let age: Int = age, age > 19, name.isEmpty == false else {
+//        print("You are too young to enter the club")
+//        return
+//    }
+//
+//    print("Welcome \(name)")
+//}
 
-func enterClub(name: String?, age: Int?) {
-    guard let name: String = name, let age: Int = age, age > 19, name.isEmpty == false else {
-        print("You are too young to enter the club")
-        return
-    }
-    
-    print("Welcome \(name)")
+let first: Int = 3
+let second: Int = 5
+
+guard first > second else {
+    // 여기에 들어올 제어문 전환 명령은 딱히 없습니다. 오류!
 }
