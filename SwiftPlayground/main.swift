8e23cbc9fd7d3b29538fb8fd6f8f76cf521c91ac
sobamemil <sieh96@naver.com>
2021-03-06 12:41:38 +0900

    리듀스 활용 예제들 및 map과 filter로의 변형 예제

--- SwiftPlayground/main.swift
+++ SwiftPlayground/main.swift
@@ -1046,9 +1046,65 @@ let numbers: [Int] = [1, 2, 3]
 //print(subtractFromThree) // -3
 
 //// 문자열 배열을 reduce(_:_:) 메소드를 이용해 연결
-//let names: [String] = ["Chope", "Jay", "Joker", "Nova"]
+let names: [String] = ["Chope", "Jay", "Joker", "Nova"]
 //
 //let reduceNames: String = names.reduce("sim's friends = ") {
 //    $0 + ", " + $1
 //}
 //print(reduceNames)
+
+// 두 번째 형태의 reduce(into:_:) 메소드 사용
+
+// 초깃값이 0이고 정수 배열의 모든 값을 더함
+// 첫 번째 리듀스 형태와 달리 클로저의 값을 반환하지 않고 내부에서 직접 이전 값을 변경한다는 점이 다름
+let sum = numbers.reduce(into: 0, { (result: inout Int, next: Int) in
+    print("\(result) + \(next)")
+    // 0 + 1
+    // 1 + 2
+    // 2 + 3
+    result += next
+})
+print(sum) // 6
+
+// 초깃값이 3이고 정수 배열의 모든 값을 뺌
+// 첫 번째 리듀스 형태와 달리 클로저의 값을 반환하지 않고 내부에서 직접 이전 값을 변경한다는 점이 다름
+let subtractFromThree = numbers.reduce(into: 3, { (result: inout Int, next: Int) in
+    print("\(result) - \(next)")
+    // 3 - 1
+    // 2 - 2
+    // 0 - 3
+    result -= next
+})
+print(subtractFromThree) // -3
+
+// 첫 번째 리듀스 형태와 다르므로 다른 컨테이너에 값을 변경하여 넣어줄 수도 있음
+// 이렇게 하면 맵이나 필터와 유사한 형태로 사용할 수도 있음
+// 홀수는 걸러내고 짝수만 두 배로 변경하여 초깃값인 [1, 2, 3] 배열에 직접 연산
+var doubledNumbers: [Int] = numbers.reduce(into: [1, 2], { (result: inout [Int], next: Int) in
+    print("result: \(result) next: \(next)")
+    
+    guard next.isMultiple(of: 2) else {
+        return
+    }
+    
+    print("\(result) append \(next * 2)")
+    
+    result.append(next * 2)
+})
+print(doubledNumbers)
+
+// 필터와 맵을 사용한 모습
+doubledNumbers = [1, 2] + numbers.filter({ (result: Int) -> Bool in return result.isMultiple(of: 2)}).map({ (result: Int) -> Int in return result * 2})
+print(doubledNumbers)
+
+// 이름을 모두 대문자로 변환하여 초깃값인 빈 배열에 직접 연산
+var upperCasedNames = names.reduce(into: [], { (result: inout [String], next: String) in
+    result.append(next.uppercased())
+})
+print(upperCasedNames)
+
+// 맵을 사용한 모습
+upperCasedNames = names.map({ (result: String) -> String in
+    result.uppercased()
+})
+print(upperCasedNames)
